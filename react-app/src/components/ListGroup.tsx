2730aba0a472c90439714989904f75479c8f1b19
Ahmed <re.90@live.com>
2025-01-14 16:17:54 +0530

    using map on a list of items

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 const ListGroup = () => {
+  const items = ["Delhi", "New York", "Mumbai", "Bangalore", "Tokyo"];
+
   return (
     <>
       <h1>List Items</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
+        {/* <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        <li className="list-group-item">And a fifth one</li> */}
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
