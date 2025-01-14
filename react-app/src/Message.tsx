454a1d1cd49cab87203f9806f73dc57f7741f58c
Ahmed <re.90@live.com>
2025-01-14 14:24:34 +0530

    add bootstrap css

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -4,13 +4,13 @@ const Message = () => {
   const name = "Dosh";
   return (
     <div
-      style={{
-        color: "red",
-        fontWeight: "bold",
-        fontSize: "24px",
-        fontStyle: "italic",
-        transform: "rotate(5deg)",
-      }}
+    // style={{
+    //   color: "red",
+    //   fontWeight: "bold",
+    //   fontSize: "24px",
+    //   fontStyle: "italic",
+    //   transform: "rotate(5deg)",
+    // }}
     >
       <h1>Hello {name ? name : "World"}</h1>
     </div>
