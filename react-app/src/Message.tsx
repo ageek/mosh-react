5df398d765c533de91e162eea9f1518646134cf3
Ahmed <re.90@live.com>
2025-01-14 12:33:13 +0530

    basic example component

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Message = () => {
+  const name = "Dosh";
+  return (
+    <div
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        fontSize: "24px",
+        fontStyle: "italic",
+        transform: "rotate(5deg)",
+      }}
+    >
+      <h1>Hello {name ? name : "World"}</h1>
+    </div>
+  );
+};
+
+export default Message;
